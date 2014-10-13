e7b33a03db03ef230c7b03406531aa52e817bc7f
Nick Litwin <nicklitwin88@gmail.com>
2014-10-13 16:00:26 -0700

    Add db.js - Center content div

--- app/views/editView.js
+++ app/views/editView.js
@@ -1,11 +1,12 @@
 var EditView = Backbone.View.extend({
+  
   initialize: function() {
     this.render();
   },
 
   render: function() {
-    p('in render');
     var $test = $('<h1>CONTENT</h1>');
+    $('.content').html('');
     $('.content').append($test);
   }
 });
