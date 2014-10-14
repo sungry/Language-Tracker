ffa72cd949494cd332c1d3b7edeb4cef11c12e04
Nick Litwin <nicklitwin88@gmail.com>
2014-10-14 16:31:52 -0700

    Update look

--- app/views/HomeView.js
+++ app/views/HomeView.js
@@ -7,27 +7,31 @@ var HomeView = Backbone.View.extend({
   },
 
   render: function(){
-    var $welcome = $('<h1>Welcome, Language Enthusiast!</h1>' +
-      '<br>' +
-      '<h2>Click Edit to save notes or View to see them.</h3>' +
-      '<br>' +
-      '<h4>Language Tracker allows you to document your progress ' +
-      'while learning a language and keep you motivated along the ' +
-      'way! This site is under construction,' +
-      ' so stay tuned for additional features.</h4>' +
-      '<br>' +
-      '<h2>Features to come:</h2>' +
-      '<ul>' +
-        '<li>View notes by day</li>' +
-        '<li>Organize notes</li>' +
-        '<li>Track studying by: </li>' +
-          '<ul>' +
-            '<li>Reading</li>' +
-            '<li>Writing</li>' +
-            '<li>Listening</li>' +
-            '<li>Speaking</li>' +
-          '</ul>' +
-      '</ul>'
+    var $welcome = $('<div class="homepage">' +
+        '<div class="well">' +
+        '<h1>Welcome, Language Enthusiast!</h1>' +
+        '<br>' +
+        '<h2>*Click Edit to save notes or View to see them*</h3>' +
+        '<br>' +
+        '<h4>Language Tracker allows you to document your progress ' +
+        'while learning a language and keep you motivated along the ' +
+        'way! This site is under construction,' +
+        ' so stay tuned for additional features.</h4>' +
+        '<br>' +
+        '<h2>Features to come:</h2>' +
+        '<ul>' +
+          '<li>View notes by day</li>' +
+          '<li>Organize notes</li>' +
+          '<li>Track studying by: </li>' +
+            '<ul>' +
+              '<li>Reading</li>' +
+              '<li>Writing</li>' +
+              '<li>Listening</li>' +
+              '<li>Speaking</li>' +
+            '</ul>' +
+        '</ul>' +
+        '</div>' +
+      '</div>'
     );
     this.$el.append($welcome);
   }
--- app/views/NotesModelView.js
+++ app/views/NotesModelView.js
@@ -7,7 +7,16 @@ var NotesModelView = Backbone.View.extend({
     this.render();
   },
 
-  template: _.template('<h3><%= type %>:<h3><p><%= body %></p><br>'),
+  template: _.template(''+
+    '<div class="panel panel-info">' +
+      '<div class="panel-heading">' +
+        '<h3 class="panel-title"><%= type %>:</h3>' +
+      '</div>' +
+      '<div class="panel-body">' +
+        '<p><%= body %></p><br>' +
+      '</div>' +
+    '</div>'
+  ),
 
   render: function(){
     this.$el.append(this.template(this.model.attributes));
