409830dc8a527472b53a258aef55b3c14dcbf256
Nick Litwin <nicklitwin88@gmail.com>
2014-10-21 15:59:39 -0700

    Create beginning of express routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-server.js
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send("My first server with express!");
+});
+
+app.get('/notes', function(req, res) {
+  res.send("Here are your notes");
+});
+
+app.get('/*', function(req, res) {
+  res.redirect('/');
+});
+
+var server = app.listen(3141, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Listening at http://%s:%s", host, port);
+});
