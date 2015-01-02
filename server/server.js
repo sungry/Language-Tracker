0695fda25f629b937252f4663f78aa121ebbaf05
Nick Litwin <nicklitwin88@gmail.com>
2015-01-01 19:37:14 -0800

    Update server to allow html5 pushstate

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var logger = require('morgan');
 
+app.use(logger('dev'));
+
+// TO DO: compress static files
 // app.use(express.compress());
+
 app.use(express.static(path.join(__dirname, '../public'), {maxAge: '1d'}));
 
-app.get('/', function(req, res) {
-  res.send("My first server with express!");
-});
-
-app.get('/notes', function(req, res) {
-  res.send("Here are your notes");
-});
-
-app.get('/*', function(req, res) {
-  res.redirect('/');
+// Enable Angular to get rid of /#/ in url paths
+app.all('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 var server = app.listen(3141, function(){
