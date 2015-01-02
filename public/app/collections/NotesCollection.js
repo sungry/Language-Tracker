9d7463d4a0725ed26c4d606904e1d1fcb73ff359
Nick Litwin <nicklitwin88@gmail.com>
2015-01-01 19:35:45 -0800

    Remove old files

--- public/app/collections/NotesCollection.js
+++
@@ -1,4 +0,0 @@
-var NotesCollection = Backbone.Firebase.Collection.extend({
-  model: NotesModel,
-  firebase: new Firebase('https://language-tracker.firebaseio.com/')
-});
--- public/app/views/ViewView.js
+++
@@ -1,8 +0,0 @@
-var ViewView = Backbone.View.extend({
-
-  initialize: function() {
-    $('.content').html('');
-    // Populate Notes Collection with Notes Models from Firebase
-    new NotesCollectionView({collection: this.collection});
-  }
-});
