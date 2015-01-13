3119758877ed19c5d0e1fee6fab887db4f649739
Nick Litwin <nicklitwin88@gmail.com>
2015-01-13 00:36:24 -0800

    Add navbar controller

---
+++ public/app/navbar/navbar.controller.js
@@ -0,0 +1,14 @@
+(function(){
+  "use strict";
+
+  angular.module('app')
+
+  .controller('NavController', NavController);
+
+  NavController.$inject = ['$scope'];
+
+  function NavController ($scope) {
+    // remove scope parameter
+    console.log('NavController initialized.');
+  }
+})();
